f5dcc17c74abfc9659c09a2f93a3b5a0b60cc20d
aixuexi <aixuexi688@gmail.com>
2026-02-25 19:42:54 -0800

    add public

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // Use relative paths so it works on GitHub Pages / OSS / any sub-path.
+  base: './',
   plugins: [react()],
 })
 
